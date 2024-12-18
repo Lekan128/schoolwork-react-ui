85010258e72dd9c9104932db034ba1c541f94f86
Olalekan Amoo <olalekanamoo128@gmail.com>
2024-12-19 00:14:46 +0100

    Add notification to entity creation

--- src/Level/CreateLevel.tsx
+++ src/Level/CreateLevel.tsx
@@ -4,12 +4,26 @@ import "../App.css";
 import { Global } from "../Util/Global";
 import MultiInput from "../components/MultiInput";
 import { useNavigate } from "react-router-dom";
+import { NotificationType } from "../Entities/Notification.type";
+import NotificationPopup from "../components/NotificationPopup";
 
 const CreateLevel = () => {
   let navitate = useNavigate();
+
+  const [notification, setNotification] = useState("");
+  const [notificationType, setNotificationType] =
+    useState<NotificationType>("primary");
+
   const [levels, setLevels] = useState<string[]>([]);
 
   const handleSave = () => {
+    if (levels.length === 0) {
+      handleSetNotificaton("Input level", "warning");
+      return;
+    }
+
+    handleSetNotificaton("Loading");
+
     fetch(Global.base_url + Global.level, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -20,8 +34,22 @@ const CreateLevel = () => {
         navitate(Global.create);
       })
       .catch((error) => {
+        handleSetNotificaton("An error occoured: " + error, "danger");
         console.log(error);
       });
+  };
+
+  const handleSetNotificaton = (
+    notification: string,
+    notificationType: NotificationType = "primary"
+  ) => {
+    setNotification(notification);
+    setNotificationType(notificationType);
+  };
+
+  const handleClearNotification = () => {
+    setNotification("");
+    setNotificationType("primary");
   };
 
   return (
@@ -30,6 +58,15 @@ const CreateLevel = () => {
       <MultiInput placeHolder="Level" onListChange={setLevels} />
       <br />
       <Button onClick={handleSave}>Save</Button>
+
+      {/* notification */}
+      {notification && (
+        <NotificationPopup
+          message={notification}
+          type={notificationType}
+          onClose={() => handleClearNotification()}
+        />
+      )}
     </>
   );
 };
